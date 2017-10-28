0d8fb7c7f8371c6bbdb153051a3055a0da08781c
connelly7 <chiannerose@gmail.com>
2017-10-28 18:06:39 -0400

    Welcome page
    Added the welcome page and the action to go to login page

--- FoodHack/app/src/main/java/com/example/chianne/foodhack/WelcomePage.java
+++ FoodHack/app/src/main/java/com/example/chianne/foodhack/WelcomePage.java
@@ -1,13 +1,36 @@
 package com.example.chianne.foodhack;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class WelcomePage extends AppCompatActivity {
+public class WelcomePage extends AppCompatActivity implements View.OnClickListener {
+
+    private Button loginHome;
+    private Button registerHome;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_page);
+
+        //set up buttons
+        loginHome = (Button) findViewById(R.id.loginHome);
+        registerHome = (Button) findViewById(R.id.registerHome);
+
+        loginHome.setOnClickListener(this);
+        registerHome.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v==loginHome) {
+            Intent intent = new Intent(WelcomePage.this, LogOnActivity.class);
+            WelcomePage.this.startActivity(intent);
+            finish();
+        }
     }
 }
