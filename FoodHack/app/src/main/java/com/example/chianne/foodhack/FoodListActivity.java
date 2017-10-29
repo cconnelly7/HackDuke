050b38bd5481aea833aa7716e2626e2655abbf69
connelly7 <chiannerose@gmail.com>
2017-10-29 05:53:25 -0400

    Button Visibility
    w probssss

--- FoodHack/app/src/main/java/com/example/chianne/foodhack/FoodListActivity.java
+++ FoodHack/app/src/main/java/com/example/chianne/foodhack/FoodListActivity.java
@@ -9,18 +9,31 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
-public class FoodListActivity extends Fragment {
+public class FoodListActivity extends Fragment implements View.OnClickListener {
     Context c;
+    private Button cart;
+    private Button checkout;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         c = container.getContext();
-
+        View v =  inflater.inflate(R.layout.activity_profile, container, false);
+        cart = (Button) v.findViewById(R.id.cart_button);
+        checkout = (Button) v.findViewById(R.id.checkout_button);
+        cart.setOnClickListener(this);
         return inflater.inflate(R.layout.activity_food_list, container, false);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == cart) {
+            checkout.setVisibility(View.VISIBLE);
+        }
     }
 
 }
