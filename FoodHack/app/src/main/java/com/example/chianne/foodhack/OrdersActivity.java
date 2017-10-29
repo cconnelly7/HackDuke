7476ff975241ac2b655e473bd658aed759b84020
Chris Battista <cbattista19@gmail.com>
2017-10-28 22:57:16 -0400

    work on orders page

--- FoodHack/app/src/main/java/com/example/chianne/foodhack/OrdersActivity.java
+++ FoodHack/app/src/main/java/com/example/chianne/foodhack/OrdersActivity.java
@@ -2,17 +2,39 @@ package com.example.chianne.foodhack;
 
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 public class OrdersActivity extends Fragment {
 
     @Nullable
+
+
+    FrameLayout frame;
+    View inflatedView;
+    int CONTENT_VIEW_ID = 42;
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        inflatedView = inflater.inflate(R.layout.fragment_request_submission, container, false);
+        FrameLayout frame = (FrameLayout) inflatedView.findViewById(R.id.ordersFrame);
+        frame.setId(CONTENT_VIEW_ID);
+        getActivity().setContentView(frame, new FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT));
+
+        if (savedInstanceState == null) {
+            Fragment newFragment = new RequestSubmissionFragment();
+            FragmentTransaction ft = getFragmentManager().beginTransaction();
+            ft.add(CONTENT_VIEW_ID, newFragment).commit();
+        }
+
         return inflater.inflate(R.layout.activity_orders, container, false);
     }
 
