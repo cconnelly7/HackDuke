8686773ba9930caac3566c58a099418df13213d5
Matt Hannan <matthewh2@ufl.edu>
2017-10-29 10:22:15 -0400

    Auto stash before merge of "master" and "origin/master"

--- FoodHack/app/src/main/java/com/example/chianne/foodhack/LoadInScreen.java
+++ FoodHack/app/src/main/java/com/example/chianne/foodhack/LoadInScreen.java
@@ -1,7 +1,12 @@
 package com.example.chianne.foodhack;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class LoadInScreen extends AppCompatActivity {
 
@@ -9,5 +14,22 @@ public class LoadInScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load_in_screen);
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageView5);
+
+        Picasso.with(this)
+                .load("drawable/nourish")
+                .into(imageView);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent i = new Intent(getApplicationContext(), DashboardActivity.class);
+                startActivity(i);
+            }
+        }, 2000);
+
     }
+
 }
