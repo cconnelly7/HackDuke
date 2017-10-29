b86bc9f9f29af08d6783fca607caa96157d1744b
connelly7 <chiannerose@gmail.com>
2017-10-29 11:06:30 -0400

    Dashboard Prettier

--- FoodHack/app/src/main/java/com/example/chianne/foodhack/DashboardActivity.java
+++ FoodHack/app/src/main/java/com/example/chianne/foodhack/DashboardActivity.java
@@ -1,21 +1,29 @@
 package com.example.chianne.foodhack;
 
 import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class DashboardActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button profileBtn;
     private Button orderBtn;
+    //private ImageView triangles;
     //private Button mapBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        //triangles = (ImageView) findViewById(R.id.background);
+        //Picasso.with(this).load(R.drawable.triangles).resize(900,1200).into(triangles);
 
 
         profileBtn = (Button) findViewById(R.id.profileBtn);
